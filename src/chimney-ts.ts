84443b64a1307d10ca7d54bc3e7432f5f2507493
airtoxin <airtoxin@icloud.com>
2019-11-16 06:58:44 +0900

    Refactor

--- src/chimney-ts.ts
+++ src/chimney-ts.ts
@@ -1,15 +1,9 @@
-type Intersection<T extends object, U extends object> = Pick<T, Extract<keyof T, keyof U> & Extract<keyof U, keyof T>>;
-type SetDifference<A, B> = A extends B ? never : A;
-type Diff<T extends object, U extends object> = Pick<T, SetDifference<keyof T, keyof U>>;
-type Assign<T extends object, U extends object, I = Diff<T, U> & Intersection<U, T> & Diff<U, T>> = Pick<I, keyof I>;
-type PickByValue<T, ValueType> = Pick<T, {
-  [Key in keyof T]: T[Key] extends ValueType ? Key : never;
-}[keyof T]>;
+import { Assign, KeyByValueType } from "./type-helpers";
+
 type SameFieldValueKeyInB<A extends {}, B extends {}, AFieldName extends keyof A> = KeyByValueType<
   B,
   A[AFieldName]
   >;
-type KeyByValueType<Obj extends {}, ValueType> = keyof PickByValue<Obj, ValueType>;
 
 type TransType<Into extends {}, From extends {}> = From extends Into
   ? TransformableTransformer<Into, From>
---
+++ src/type-helpers.ts
@@ -0,0 +1,8 @@
+export type Intersection<T extends object, U extends object> = Pick<T, Extract<keyof T, keyof U> & Extract<keyof U, keyof T>>;
+export type SetDifference<A, B> = A extends B ? never : A;
+export type Diff<T extends object, U extends object> = Pick<T, SetDifference<keyof T, keyof U>>;
+export type Assign<T extends object, U extends object, I = Diff<T, U> & Intersection<U, T> & Diff<U, T>> = Pick<I, keyof I>;
+export type PickByValue<T, ValueType> = Pick<T, {
+  [Key in keyof T]: T[Key] extends ValueType ? Key : never;
+}[keyof T]>;
+export type KeyByValueType<Obj extends {}, ValueType> = keyof PickByValue<Obj, ValueType>;
