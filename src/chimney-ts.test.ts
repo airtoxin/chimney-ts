ee3c7c7bd8d96aa921a03b998691ab65a465f696
airtoxin <airtoxin@icloud.com>
2019-11-17 06:18:34 +0900

    Support T[]

--- src/chimney-ts.test.ts
+++ src/chimney-ts.test.ts
@@ -14,26 +14,28 @@ export function assertType<T extends true>() {
 
 type Transformable<A> = TypeHas<A, "transform">;
 
+
+const square = { size: 10 };
+const rect = { width: 10, height: 10 };
+const coloredRect = { width: 10, height: 10, color: "red" };
+
+type Square = typeof square;
+type Rect = typeof rect;
+type ColoredRect = typeof coloredRect;
+
 describe("Chimney", () => {
-  const square = { size: 10 };
-  const rect = { width: 10, height: 10 };
-  const coloredRect = { width: 10, height: 10, color: "red" };
-
-  type Square = typeof square;
-  type Rect = typeof rect;
-  type ColoredRect = typeof coloredRect;
-
   it("From/Into types are exactly equal type", () => {
     const result = new Chimney(rect).into<Rect>().transform();
     type Result = typeof result;
     assertType<TypeEq<Result, Rect>>();
+    expect(result).toEqual(rect);
   });
 
   it("From type compatible to Into type", () => {
-    const coloredRect = { ...rect, color: "red" };
     const result = new Chimney(coloredRect).into<Rect>().transform();
     type Result = typeof result;
     assertType<TypeEq<Result, Rect>>();
+    expect(result).toEqual(coloredRect);
   });
 
   it("can't call transform method if From type not compatible to Into type", () => {
@@ -50,6 +52,7 @@ describe("Chimney", () => {
       .transform();
     type Result = typeof result;
     assertType<TypeEq<Result, Rect>>();
+    expect(result).toEqual({ ...square, width: 10, height: 10 })
   });
 
   it("can't call transform method if required fields are not exists at Transformer type in withFieldConst", () => {
@@ -66,6 +69,7 @@ describe("Chimney", () => {
       .transform();
     type Result = typeof result;
     assertType<TypeEq<Result, Rect>>();
+    expect(result).toEqual({ ...square, width: square.size, height: square.size });
   });
 
   it("can't call transform method if required fields are not exists at Transformer type in withFieldConst", () => {
@@ -82,10 +86,86 @@ describe("Chimney", () => {
       .transform();
     type Result = typeof result;
     assertType<TypeEq<Result, Rect>>();
+    expect(result).toEqual({ ...square, width: square.size * 10, height: square.size * 10 });
   });
 
   it("can't call transform method if required fields are not exists at Transformer type in withFieldComputed", () => {
     const transformer = new Chimney(square)
+      .into<Rect>()
+      .withFieldComputed("width", square => square.size * 10);
+    type Transformer = typeof transformer;
+    assertType<Not<Transformable<Transformer>>>();
+  });
+});
+
+describe("Chimney Array", () => {
+  it("From/Into types are exactly equal type", () => {
+    const result = new Chimney([rect]).into<Rect>().transform();
+    type Result = typeof result;
+    assertType<TypeEq<Result, Rect[]>>();
+    expect(result).toEqual([rect]);
+  });
+
+  it("From type compatible to Into type", () => {
+    const result = new Chimney([coloredRect]).into<Rect>().transform();
+    type Result = typeof result;
+    assertType<TypeEq<Result, Rect[]>>();
+    expect(result).toEqual([coloredRect]);
+  });
+
+  it("can't call transform method if From type not compatible to Into type", () => {
+    const transformer = new Chimney([square]).into<Rect>();
+    type Transformer = typeof transformer;
+    assertType<Not<Transformable<Transformer>>>();
+  });
+
+  it("From type transform with withFieldConst to Into type", () => {
+    const result = new Chimney([square])
+      .into<Rect>()
+      .withFieldConst("width", 10)
+      .withFieldConst("height", 10)
+      .transform();
+    type Result = typeof result;
+    assertType<TypeEq<Result, Rect[]>>();
+    expect(result).toEqual([{ ...square, width: 10, height: 10 }]);
+  });
+
+  it("can't call transform method if required fields are not exists at Transformer type in withFieldConst", () => {
+    const transformer = new Chimney([square]).into<Rect>().withFieldConst("width", 10);
+    type Transformer = typeof transformer;
+    assertType<Not<Transformable<Transformer>>>();
+  });
+
+  it("From type transform with withFieldRenamed to Into type", () => {
+    const result = new Chimney([square])
+      .into<Rect>()
+      .withFieldRenamed("size", "width")
+      .withFieldRenamed("size", "height")
+      .transform();
+    type Result = typeof result;
+    assertType<TypeEq<Result, Rect[]>>();
+    expect(result).toEqual([{ ...square, width: square.size, height: square.size }]);
+  });
+
+  it("can't call transform method if required fields are not exists at Transformer type in withFieldConst", () => {
+    const transformer = new Chimney([square]).into<Rect>().withFieldRenamed("size", "width");
+    type Transformer = typeof transformer;
+    assertType<Not<Transformable<Transformer>>>();
+  });
+
+  it("From type transform with withFieldComputed to Into type", () => {
+    const result = new Chimney([square])
+      .into<Rect>()
+      .withFieldComputed("width", square => square.size * 10)
+      .withFieldComputed("height", square => square.size * 10)
+      .transform();
+    type Result = typeof result;
+    assertType<TypeEq<Result, Rect[]>>();
+    expect(result).toEqual([{ ...square, width: square.size * 10, height: square.size * 10 }]);
+  });
+
+  it("can't call transform method if required fields are not exists at Transformer type in withFieldComputed", () => {
+    const transformer = new Chimney([square])
       .into<Rect>()
       .withFieldComputed("width", square => square.size * 10);
     type Transformer = typeof transformer;
--- src/chimney-ts.ts
+++ src/chimney-ts.ts
@@ -5,54 +5,90 @@ type SameFieldValueKeyInB<A extends {}, B extends {}, AFieldName extends keyof A
   A[AFieldName]
   >;
 
-type TransType<Into extends {}, From extends {}> = From extends Into
+type TransType<Into extends {}, From extends {} | {}[]> = From extends Into
   ? TransformableTransformer<Into, From>
-  : Transformer<Into, From>;
+  : From extends Array<infer RFrom>
+    ? RFrom extends Into ?
+      TransformableTransformer<Into, RFrom[]> : Transformer<Into, From>
+    : Transformer<Into, From>;
 
-class Transformer<Into extends {}, From extends {}> {
+class Transformer<Into extends {}, From extends {} | {}[]> {
   constructor(protected fromObj: From) {}
 
   withFieldConst<
     IntoFieldName extends keyof Into,
-    NextFrom extends Assign<From, Record<IntoFieldName, Into[IntoFieldName]>>
+    NextFrom extends
+      From extends Array<infer RFrom>
+        ? RFrom extends {}
+          ? Assign<RFrom, Record<IntoFieldName, Into[IntoFieldName]>>[]
+          : never
+        : Assign<From, Record<IntoFieldName, Into[IntoFieldName]>>
   >(intoFieldName: IntoFieldName, constValue: Into[IntoFieldName]): TransType<Into, NextFrom> {
-    return new TransformableTransformer({ ...this.fromObj, [intoFieldName]: constValue }) as any;
+    if (Array.isArray(this.fromObj)) {
+      return new TransformableTransformer(this.fromObj.map(el => ({ ...el, [intoFieldName]: constValue }))) as any;
+    } else {
+      return new TransformableTransformer({ ...this.fromObj, [intoFieldName]: constValue }) as any;
+    }
   }
 
   withFieldRenamed<
-    FromFieldName extends keyof From,
+    FromFieldName extends From extends Array<infer RFrom> ? keyof RFrom : keyof From,
     IntoFieldName extends SameFieldValueKeyInB<From, Into, FromFieldName>,
-    NextFrom extends Assign<From, Record<IntoFieldName, Into[IntoFieldName]>>
+    NextFrom extends
+      From extends Array<infer RFrom>
+        ? RFrom extends {}
+        ? Assign<RFrom, Record<IntoFieldName, Into[IntoFieldName]>>[]
+        : never
+        : Assign<From, Record<IntoFieldName, Into[IntoFieldName]>>
   >(fromFieldName: FromFieldName, intoFieldName: IntoFieldName): TransType<Into, NextFrom> {
-    return new TransformableTransformer({
-      ...this.fromObj,
-      [intoFieldName]: this.fromObj[fromFieldName]
-    }) as any;
+    if (Array.isArray(this.fromObj)) {
+      return new TransformableTransformer(this.fromObj.map(el => ({
+        ...el,
+        [intoFieldName]: el[fromFieldName]
+      }))) as any;
+    } else {
+      return new TransformableTransformer({
+        ...this.fromObj,
+        [intoFieldName]: this.fromObj[fromFieldName]
+      }) as any;
+    }
   }
 
   withFieldComputed<
     IntoFieldName extends keyof Into,
-    Computer extends (v: From) => Into[IntoFieldName],
-    NextFrom extends Assign<From, Record<IntoFieldName, Into[IntoFieldName]>>
-  >(intoFieldName: IntoFieldName, computeFn: Computer): TransType<Into, NextFrom> {
-    return new TransformableTransformer({
-      ...this.fromObj,
-      [intoFieldName]: computeFn(this.fromObj)
-    }) as any;
+    ComputeFn extends (v: From extends Array<infer R> ? R : From) => Into[IntoFieldName],
+    NextFrom extends
+      From extends Array<infer RFrom>
+        ? RFrom extends {}
+        ? Assign<RFrom, Record<IntoFieldName, Into[IntoFieldName]>>[]
+        : never
+        : Assign<From, Record<IntoFieldName, Into[IntoFieldName]>>
+  >(intoFieldName: IntoFieldName, computeFn: ComputeFn): TransType<Into, NextFrom> {
+    if (Array.isArray(this.fromObj)) {
+      return new TransformableTransformer(this.fromObj.map(el => ({
+        ...el,
+        [intoFieldName]: computeFn(el)
+      }))) as any;
+    } else {
+      return new TransformableTransformer({
+        ...this.fromObj,
+        [intoFieldName]: computeFn(this.fromObj as any)
+      }) as any;
+    }
   }
 }
 
-class TransformableTransformer<Into extends {}, From extends Into> extends Transformer<Into, From> {
+class TransformableTransformer<Into extends {}, From extends Into | Into[]> extends Transformer<Into, From> {
   constructor(protected fromObj: From) {
     super(fromObj);
   }
 
-  transform(): Into {
-    return this.fromObj;
+  transform(): From extends Into ? Into : Into[] {
+    return this.fromObj as any;
   }
 }
 
-export class Chimney<From extends {}> {
+export class Chimney<From extends {} | {}[]> {
   constructor(private fromObj: From) {}
 
   into<Into extends {}>(): TransType<Into, From> {
